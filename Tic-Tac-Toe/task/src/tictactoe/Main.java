8b6dbfc7065a5059a018a7a33fa75d195afe16b2
otakun40 <67707776+otakun40@users.noreply.github.com>
2020-07-30 14:04:32 +0300

    Initial commit

---
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -0,0 +1,11 @@
+package tictactoe;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Field tictactoe = new Field();
+    tictactoe.play();
+    }
+}
